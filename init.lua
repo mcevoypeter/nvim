4fb6aa9d115bf3354781bcc7715b86e47315b863
Peter McEvoy <git@mcevoypeter.com>
2023-11-23 07:48:33 -0700

    Set search settings

--- init.lua
+++ init.lua
@@ -3,3 +3,14 @@ vim.cmd([[
   set splitbelow
   set splitright
 ]])
+
+-- search settings
+vim.cmd([[
+  set hlsearch
+  set incsearch
+  " Enable case-insensitive search when all letters are lowercase and
+  " case-sensitive search when some letters are uppercase.
+  set ignorecase smartcase
+  " Display search count message.
+  set shortmess-=S
+]])
