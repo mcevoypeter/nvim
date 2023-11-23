ce2e10da19d0a1133a403dbf7b225e0e8a0243b1
Peter McEvoy <git@mcevoypeter.com>
2023-11-23 09:51:02 -0700

    Add word count to status bar

--- lua/lightline-vim.lua
+++ lua/lightline-vim.lua
@@ -6,7 +6,7 @@ vim.g.lightline = {
       { "gitbranch", "readonly", "filename", "modified" }
     },
     right = {
-      { "lineinfo", "charvaluehex" },
+      { "lineinfo", "charvaluehex", "wordcount" },
       { "percent" },
       { "fileformat", "fileencoding", "filetype" }
     }
@@ -16,7 +16,8 @@ vim.g.lightline = {
     filename = "%f"
   },
   component_function = {
-    gitbranch = "FugitiveHead"
+    gitbranch = "FugitiveHead",
+    wordcount = "WordCount"
   },
   mode_map = {
     n = "N",
@@ -33,3 +34,36 @@ vim.g.lightline = {
     t= "T"
   }
 }
+
+-- from https://github.com/itchyny/lightline.vim/issues/295#issuecomment-373961016
+vim.cmd([[
+  function! WordCount()
+    let currentmode = mode()
+    if !exists("g:lastmode_wc")
+      let g:lastmode_wc = currentmode
+    endif
+    " if we modify file, open a new buffer, be in visual ever, or switch modes
+    " since last run, we recompute.
+    if &modified || !exists("b:wordcount") || currentmode =~? '\c.*v' || currentmode != g:lastmode_wc
+      let g:lastmode_wc = currentmode
+      let l:old_position = getpos('.')
+      let l:old_status = v:statusmsg
+      execute "silent normal g\<c-g>"
+      if v:statusmsg == "--No lines in buffer--"
+        let b:wordcount = 0
+      else
+        let s:split_wc = split(v:statusmsg)
+        if index(s:split_wc, "Selected") < 0
+          let b:wordcount = str2nr(s:split_wc[11])
+        else
+          let b:wordcount = str2nr(s:split_wc[5])
+        endif
+        let v:statusmsg = l:old_status
+      endif
+      call setpos('.', l:old_position)
+      return b:wordcount
+    else
+      return b:wordcount
+    endif
+  endfunction
+]])
